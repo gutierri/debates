f8bca5596216559b63ba100c0056511c2f1e9b54
Gutierri Barboza <me@gutierri.me>
2017-11-20 10:25:00 -0200

    login implementation

--- chat/urls.py
+++ chat/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import login, logout
 from django.conf.urls import url, include
 from django.contrib import admin
 from .views import room
@@ -5,4 +6,6 @@ from .views import room
 
 urlpatterns = [
     url(r'^$', room, name='room'),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def room(request):
     return render(request, 'chat/room.html')
