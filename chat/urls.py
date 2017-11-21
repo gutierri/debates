116d9b4c06394c7cc07b3a08ecab2bf5812c859b
Gutierri Barboza <me@gutierri.me>
2017-11-21 05:24:26 -0200

    list rooms homepage

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.views import login, logout
 from django.conf.urls import url, include
 from django.contrib import admin
-from .views import room
+from .views import room, list_rooms
 
 
 urlpatterns = [
+    url(r'^$', list_rooms, name='list_rooms'),
     url(r'^room/$', room, name='room'),
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, name='logout'),
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Room
+
+
+@login_required
+def list_rooms(request):
+    rooms = Room.objects.all()
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'chat/list_rooms.html', context)
 
 @login_required
 def room(request):
