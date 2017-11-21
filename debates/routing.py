34c80ba208d7036259b1ae58e3baaf4bc0af3a12
Gutierri Barboza <me@gutierri.me>
2017-11-21 06:20:31 -0200

    update settings for asgi and routing

--- debates/routing.py
+++ debates/routing.py
@@ -1,8 +1,11 @@
+from channels.staticfiles import StaticFilesConsumer
 from channels.routing import route
 from chat.consumers import ws_add, ws_message, ws_disconnect
 
-channel_routing = [
-    route("websocket.connect", ws_add),
-    route("websocket.receive", ws_message),
-    route("websocket.disconnect", ws_disconnect),
-]
+
+channel_routing = {
+    'http.request': StaticFilesConsumer(),
+    'websocket.connect': ws_add,
+    'websocket.receive': ws_message,
+    'websocket.disconnect': ws_disconnect,
+}
